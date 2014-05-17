abc8a3f717a512e9224f1fe6dc220b1ca06d9106
Lerke <lerke@berrypunch.net>
2014-05-17 16:59:03 +0200

    Fixed some crashing issues.

--- luna.js
+++ luna.js
@@ -492,7 +492,7 @@ function isShuffleEnabledForRoom(currentRoom) {
 }
 
 function Song(songname, songID, numPlayed) {
-	this.title = songname;
+	this.title = songname || '';
 	this.ID = songID;
 	this.numPlayed = numPlayed;
 }
@@ -629,7 +629,7 @@ Song.prototype.toString = function() {
 	}
 
 	//Check if new song is legit.
-	if(newSong.title == undefined || newSong.url == undefined || newSong.id == undefined) {
+	if(!newSong.title || newSong.url === undefined || newSong.url.length === 0 || newSong.id === undefined) {
 		return false; // It aint cool!
 	}
 	//Add new Song object to the current room.
@@ -837,7 +837,11 @@ function sortPlaylist(room) {
 
 	//Sort the playlist!
 	tracks.sort(function(a,b) {
-		return (a.title.toLowerCase() < b.title.toLowerCase()) ? -1 : ((a.title.toLowerCase() > b.title.toLowerCase()) ? 1 : 0);
+		//Enforce that tracks are always strings.
+		var tA = (a === null) ? "" : "" + a;
+		var tB = (b === null) ? "" : "" + b;
+		return (tA.title.toLowerCase() < tB.title.toLowerCase()) ? -1 : ((tA.title.toLowerCase() > tB.title.toLowerCase()) ? 1 : 0);
+		
 	});
 
 	//Iterate over it and replace the ID numbers.
